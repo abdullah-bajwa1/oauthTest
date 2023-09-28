269fb1617f2ee1b2dc63c69a13c95bdbbb45a440
Abdullah <145961346+abdullah-bajwa1@users.noreply.github.com>
2023-09-28 15:19:35 +0500

    annotated

--- model.py
+++ model.py
@@ -2,4 +2,7 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     username: str
-    password: str
+    password: str    #we will need to hash this for storage, passlib automatically identifies the available libraries to use for hashing (bcrypt in my case)
+
+# I have not tested with multiple libs installed as i was developing inside a venv with only bcrypt installed
+# you might have to watch out for any conflicts and need some additional config if you run with multiple encryption libraries installed on your system
